14ddb819b9c93bdeb7f7486d6d0f15dc4ebbef23
joost mulders <joost.mulders@proximus.be>
2020-11-16 11:53:20 +0100

    Bugfix

--- Fruitshop/Fruitshop/Program.cs
+++ Fruitshop/Fruitshop/Program.cs
@@ -6,7 +6,7 @@ namespace Fruitshop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Fruits!");
         }
     }
 }
