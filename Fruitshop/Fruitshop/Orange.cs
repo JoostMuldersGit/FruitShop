fc0915a90afacbd8e4eaa1d21b65a3bc4e7e2d81
joost mulders <joost.mulders@proximus.be>
2020-11-16 11:51:57 +0100

    Squeeze

--- Fruitshop/Fruitshop/Orange.cs
+++ Fruitshop/Fruitshop/Orange.cs
@@ -1,7 +1,13 @@
-﻿namespace Fruitshop
+﻿using System;
+
+namespace Fruitshop
 {
     public class Orange : Fruit
     {
-        
+        public string Squeeze()
+        {
+            Console.WriteLine("Squeez");
+            return "Ok";
+        }
     }
 }
