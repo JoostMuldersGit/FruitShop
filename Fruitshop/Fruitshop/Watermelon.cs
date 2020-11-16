02b18386bb9fbdacd19077d7b051918a4593040a
joost mulders <joost.mulders@proximus.be>
2020-11-16 12:04:53 +0100

    Added method Cut() in Watermelon class.

--- Fruitshop/Fruitshop/Watermelon.cs
+++ Fruitshop/Fruitshop/Watermelon.cs
@@ -1,7 +1,12 @@
-﻿namespace Fruitshop
+﻿using System;
+
+namespace Fruitshop
 {
     public class Watermelon : Fruit
     {
-        
+        public void Cut()
+        {
+            Console.WriteLine("Cutting");
+        }
     }
 }
