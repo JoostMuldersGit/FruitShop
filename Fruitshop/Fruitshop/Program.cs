a323851eccb8100b109daa889beb1f05f28a3eb3
joost mulders <joost.mulders@proximus.be>
2020-11-16 11:56:29 +0100

    Hi

--- Fruitshop/Fruitshop/Program.cs
+++ Fruitshop/Fruitshop/Program.cs
@@ -6,7 +6,8 @@ namespace Fruitshop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Fruits!");
+            Console.WriteLine("Hi Fruits!");
+            Console.WriteLine("----------");
         }
     }
 }
