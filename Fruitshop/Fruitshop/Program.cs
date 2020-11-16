e9f34525d7756504a050e77e85266eaa6e1a08a0
joost mulders <joost.mulders@proximus.be>
2020-11-16 11:57:08 +0100

    Stars

--- Fruitshop/Fruitshop/Program.cs
+++ Fruitshop/Fruitshop/Program.cs
@@ -7,6 +7,7 @@ namespace Fruitshop
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Fruits!");
+            Console.WriteLine("*************");
         }
     }
 }
